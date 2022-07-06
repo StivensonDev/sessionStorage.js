9abf896a0209e1e55a6e821aafa0f9f5a7579c50
STIVENSON.DEV <96761629+StivensonSS@users.noreply.github.com>
2022-07-05 21:12:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const apellido = "Sosa"
 
 const objeto = (`${nombre} ${apellido}`)
 
-console.log(objeto)
+// sessionStorage.setItem("objeto", JSON.stringify(objeto))
+// localStorage.setItem("objeto", JSON.stringify(objeto))
+
+const hora = new Date()
+// document.cookie = `datos=${JSON.stringify(objeto)};expires=${new Date(now.getTime() + 2 * 60000)}`
